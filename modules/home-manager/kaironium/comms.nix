f5ec34be275b2561e7d40ee9930db44383efaa8e
kaironium <kai.svenson628@gmail.com>
2026-02-20 18:06:57 -0600

    fixed typos

--- modules/home-manager/kaironium/comms.nix
+++ modules/home-manager/kaironium/comms.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }: {
   home.packages = with pkgs; [
     telegram-desktop
+    discord
+    zoom-us
   ];
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elm (pkgs.lib.getName pkg) [
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
     "discord"
-    "zoom-us"
+    "zoom"
   ];
 }
