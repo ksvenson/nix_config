55c245063995268e27575fa3685994dbbcb4f5fe
kaironium <kai.svenson628@gmail.com>
2026-03-05 08:39:23 -0600

    saving changes

--- modules/flake/machine_factory.nix
+++ modules/flake/machine_factory.nix
@@ -1,19 +1,19 @@
-{ inputs, config, ...}: {
+{ inputs, self, config, ...}: {
   let
-    buildNixosSystem = machine: inputs.nixpkgs.lib.nixosSystem {
-      inherit machine.system;
+    buildNixosSystem = machineInfo: inputs.nixpkgs.lib.nixosSystem {
+      inherit machineInfo.system;
       specialArgs = {
-        inherit machine;  # TODO: this may not be necessary - here tentatively to automatically build owner user
+        inherit machineInfo;  # TODO: this may not be necessary - here tentatively to automatically build owner user
         modulesPath = "${inputs.nixpkgs}/nixos/modules";  # TODO: find out if this is necessary
         # TODO: may later become necessary to `inherit inputs`
       };
       modules = [
-        config.flake.nixosModules.common
-        config.flake.nixosModules.${machine.hostName}
+        (import-tree ${self}/modules/common)
+        (import-tree ${self}/modules/machines/${machineInfo.hostName})
       ];
     };
-  in {
-    config.flake.nixosConfigurations = builtins.map (buildNixosSystem) config.flake.machines
 
+  in {
+    flake.nixosConfigurations = builtins.map (buildNixosSystem) config.flake.machines  # FIXME this should be an attribute set, not list
   }
 }
