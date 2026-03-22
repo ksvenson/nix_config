d8a273fabf491f7d4bdbb7399b928cef9a7ea601
kaironium <kai.svenson628@gmail.com>
2026-03-22 13:42:57 -0500

    fixed bugs with previous commit

--- modules/users/kaironium/i3/keybinds.nix
+++ modules/users/kaironium/i3/keybinds.nix
@@ -2,38 +2,38 @@
   let
     mod = "Mod1";
     arrows = {
-      "k" = "up";
-      "l" = "down";
-      "j" = "left";
-      "semicolon" = "right";
+      "up" = "k";
+      "down" = "l";
+      "left" = "j";
+      "right" = "semicolon";
     };
   in {
     xsession.windowManager.i3.config = {
       modifier = mod;
 
-      terminal = "kitty";
-
-      keybindings = lib.mkOptionDefault {
+      keybindings = lib.mkOptionDefault (
+      (lib.mapAttrs' (dir: key: lib.nameValuePair ("${mod}+${key}") ("focus ${dir}")) arrows)
+      //
+      (lib.mapAttrs' (dir: key: lib.nameValuePair ("${mod}+Shift+${key}") ("move ${dir}")) arrows)
+      //
+      (lib.mapAttrs' (num: label: lib.nameValuePair ("${mod}+${num}") ("workspace ${label}")) config.i3.workspaces)
+      //
+      (lib.mapAttrs' (num: label: lib.nameValuePair ("${mod}+Shift+${num}") ("move container to workspace ${label}")) config.i3.workspaces)
+      //
+      {
         "${mod}+Shift+p" = "exec rofi -show power-menu -modi 'power-menu:${pkgs.rofi-power-menu}/bin/rofi-power-menu'";
         "${mod}+d" = "exec rofi -show drun";
 
         "${mod}+a" = "focus parent";
         "${mod}+z" = "focus child";
       }
-      \\
-      (lib.mapAttrs' (key: dir: lib.nameValuePair ("${mod}+${key}") ("focus ${dir}")) arrows)
-      \\
-      (lib.mapAttrs' (dir: key: lib.nameValuePair ("${mod}+Shift+${key}") ("move ${dir}")) arrows)
-      \\
-      (lib.mapAttrs' (num: label: lib.nameValuePair ("${mod}+${num}") ("workspace ${label}")) config.i3.workspaces)
-      \\
-      (lib.mapAttrs' (num: label: lib.nameValuePair ("${mod}+Shift+${num}") ("move container to workspace ${label}")) config.i3.workspaces);
+      );
 
       modes.resize = {
-        "${u}" = "resize grow height 10 px or 10 ppt";
-        "${d}" = "resize shrink height 10 px or 10 ppt";
-        "${l}" = "resize shrink width 10 px or 10 ppt";
-        "${r}" = "resize grow width 10 px or 10 ppt";
+        "${arrows.up}" = "resize grow height 10 px or 10 ppt";
+        "${arrows.down}" = "resize shrink height 10 px or 10 ppt";
+        "${arrows.left}" = "resize shrink width 10 px or 10 ppt";
+        "${arrows.right}" = "resize grow width 10 px or 10 ppt";
 
         "Escape" = "mode default";
         "Return" = "mode default";
--- modules/users/kaironium/i3/workspaces.nix
+++ modules/users/kaironium/i3/workspaces.nix
@@ -8,9 +8,9 @@
   options.i3.workspaces = mkOption {
     type = types.attrsOf types.str;
     default = {
-      "1" = "1: Terminals"
-      "2" = "2: Browsers"
-      "3" = "3: Comms"
+      "1" = "1: Terminals";
+      "2" = "2: Browsers";
+      "3" = "3: Comms";
     };
   };
 }
