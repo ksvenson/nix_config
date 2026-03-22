c6de0c4a7c63e079cd3b83ba0808bbf294f60ee7
kaironium <kai.svenson628@gmail.com>
2026-03-22 13:59:04 -0500

    refactoring i3/base.nix

--- modules/users/kaironium/i3/base.nix
+++ modules/users/kaironium/i3/base.nix
@@ -3,10 +3,15 @@
     enable = true;
     windowManager.i3 = {
       enable = true;
+      config = {
+        terminal = "kitty";
+        fonts = {
+          names = [ config.theme.font.name ];
+          size = config.theme.font.size + 0.0;  # needs to be a float
+        };
+      };
     };
   };
 
-  programs.i3status = {
-    enable = true;
-  };
+  programs.i3status.enable = true;
 }
--- modules/users/kaironium/i3/config.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, ... }: {
-  xsession.windowManager.i3.config = {
-    fonts = {
-      names = [ config.theme.font.name ];
-      size = config.theme.font.size + 0.0;
-    };
-  };
-}
