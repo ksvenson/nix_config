d57533820dba3fe6619e61b29c7bd8258cc4b4dd
kaironium <kai.svenson628@gmail.com>
2026-06-17 02:20:20 -0500

    setting up direnv

---
+++ modules/machines/common/_unused/dms_greeter.nix
@@ -0,0 +1,11 @@
+{ inputs, config, machine, ... }: {
+  imports = [
+    inputs.dms.nixosModules.greeter
+  ];
+  programs.dank-material-shell.greeter = {
+    enable = true;
+    compositor = {
+      name = "niri";
+    };
+  };
+}
---
+++ modules/machines/common/nix-ld.nix
@@ -0,0 +1,3 @@
+{ config, ... }: {
+  programs.nix-ld.enable = true;
+}
