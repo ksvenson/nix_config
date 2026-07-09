8c2bf95af02f606e1a257216d10728559d3a3910
kaironium <kai.svenson628@gmail.com>
2026-07-08 22:47:42 -0500

    added nix-ld for uv python projects

--- modules/machines/common/nix-ld.nix
+++ modules/machines/common/nix-ld.nix
@@ -1,4 +1,9 @@
-{ config, ... }: {
+{ pkgs, config, ... }: {
   # needed for python projects managed by uv
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      libxcrypt-legacy  # for older versions of python
+    ];
+  };
 }
