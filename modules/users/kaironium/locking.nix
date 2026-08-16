18c0a86a13c2ae7d5a9caf7fe6b2b58a4e617da9
kaironium <kai.svenson628@gmail.com>
2026-08-16 13:33:54 -0500

    fixed locking cmd

--- modules/users/kaironium/locking.nix
+++ modules/users/kaironium/locking.nix
@@ -6,7 +6,7 @@
         # turn the monitors off before locking so that the lock screen
         # doesn't flash when powering down. This is purely a cosmetic thing.
         before_sleep_cmd = "niri msg action power-off-monitors; noctalia msg session lock";
-        lock_cmd = "noctalia-shell ipc call lockScreen lock";
+        lock_cmd = "noctalia msg session lock";
       };
     };
   };
